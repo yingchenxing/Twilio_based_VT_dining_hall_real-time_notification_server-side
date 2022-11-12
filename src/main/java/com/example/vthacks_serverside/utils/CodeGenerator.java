ce2e6927b48f91f214dfe97467d85b3b4bbcde31
yingchenxing <963929975@qq.com>
2022-11-12 12:34:35 -0500

    Add config

--- src/main/java/com/example/vthacks_serverside/utils/CodeGenerator.java
+++ src/main/java/com/example/vthacks_serverside/utils/CodeGenerator.java
@@ -15,13 +15,13 @@ public class CodeGenerator {
                 .globalConfig(builder -> {
                     builder.author("xic") // 设置作者
 //                            .enableSwagger() // 开启 swagger 模式
-                            .fileOverride() // 覆盖已生成文件
+//                            .fileOverride() // 覆盖已生成文件
                             .outputDir("D:\\vthacks\\vthacks_serverside\\src\\main\\java\\"); // 指定输出目录
                 })
                 .packageConfig(builder -> {
                     builder.parent("com.example.vthacks_serverside") // 设置父包名
                             .moduleName(null) // 设置父包模块名
-                            .pathInfo(Collections.singletonMap(OutputFile.mapperXml, "D:\\AITraining\\Backend\\src\\main\\resources\\mapper\\")); // 设置mapperXml生成路径
+                            .pathInfo(Collections.singletonMap(OutputFile.mapperXml, "D:\\vthacks\\vthacks_serverside\\src\\main\\resources\\mapper\\")); // 设置mapperXml生成路径
                 })
                 .strategyConfig(builder -> {
                     builder.entityBuilder().enableLombok();
